cf998a3df614395dfd781853c217e0e75ded997b
RomanovAleksandr <61905953+RomanovAleksandr@users.noreply.github.com>
2020-09-21 14:47:00 +0300

    lab1

---
+++ lab1/Triangle/Triangle/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Triangle
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length != 3)
+            {
+                Console.WriteLine("неизвестная ошибка");
+                return;
+            }
+            try
+            {
+                double a = Double.Parse(args[0]);
+                double b = Double.Parse(args[1]);
+                double c = Double.Parse(args[2]);
+                if (a + b <= c || a + c <= b || b + c <= a || a < 0 || b < 0 || c < 0)
+                {
+                    Console.WriteLine("не треугольник");
+                }
+                else if (a != b && a != c && b != c)
+                {
+                    Console.WriteLine("обычный");
+                }
+                else if (a == b && b == c)
+                {
+                    Console.WriteLine("равносторонний");
+                }
+                else
+                {
+                    Console.WriteLine("равнобедренный");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("неизвестная ошибка");
+            }
+        }
+    }
+}
---
+++ lab1/Triangle/TriangleTests/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+
+namespace TriangleTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader input = new StreamReader(@"..\..\..\test.txt"))
+            {
+                using (StreamWriter output = new StreamWriter(@"..\..\..\output.txt"))
+                {
+                    int i = 1;
+                    string arguments;
+                    while ((arguments = input.ReadLine()) != null)
+                    {
+                        string expected = input.ReadLine();
+
+                        var proc = new Process
+                        {
+                            StartInfo = new ProcessStartInfo
+                            {
+                                FileName = "C:\\Users\\Александр\\Documents\\2020\\qa\\lab1\\Triangle\\Triangle\\bin\\Debug\\netcoreapp3.1\\Triangle.exe",
+                                Arguments = arguments,
+                                UseShellExecute = false,
+                                RedirectStandardOutput = true,
+                                CreateNoWindow = true
+                            }
+                        };
+
+                        proc.Start();
+                        StreamReader reader = proc.StandardOutput;
+                        string procOutput = reader.ReadToEnd();
+
+                        if (procOutput.Trim() == expected)
+                        {
+                            output.WriteLine(i + " success");
+                        }
+                        else
+                        {
+                            output.WriteLine(i + " error");
+                        }
+                        i++;
+                    }
+                }
+            }
+        }
+    }
+}
