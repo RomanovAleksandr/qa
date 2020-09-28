fccafcf1783933e8d45fa4044af6dd8d99d55880
RomanovAleksandr <61905953+RomanovAleksandr@users.noreply.github.com>
2020-09-28 10:12:36 +0300

    lab2

--- lab2/lab2/Program.cs
+++ lab2/lab2/Program.cs
@@ -10,40 +10,67 @@ namespace lab2
     {
         static string root = "http://91.210.252.240/broken-links/";
 
-        static void ParseLinks(string content, List<string> links)
+        static void ParseLinks(string content, List<string> links, List<string> newLinks)
         {
-            string regex = "<a.*href=\"([^#][^@]*?)\".*>";
-            foreach (var link in Regex.Matches(content, regex))
+            string regex = "<a.*href=\"(.*?)\">?";
+            foreach (Match match in Regex.Matches(content, regex))
             {
-                string url = Regex.Match(link.ToString(), regex).Groups[1].Value;
-                Console.WriteLine(url);
+                string url = match.Groups[1].Value;
+                url = root + url;
+                if (!links.Contains(url) && !newLinks.Contains(url))
+                {
+                    links.Add(url);
+                    newLinks.Add(url);
+                }
             }
         }
 
-        static List<string> Request(string link, List<string> links)
+        static void Request(string currentLink, List<string> links, List<string> newLinks, StreamWriter validLinksFile, StreamWriter invalidLinksFile)
         {
-            List<string> newLinks = new List<string>();
-            WebRequest request = WebRequest.Create(link);
-            WebResponse response = request.GetResponse();
-            string responseFromServer;
-            using (Stream dataStream = response.GetResponseStream())
+            try
             {
-                StreamReader reader = new StreamReader(dataStream);
-                responseFromServer = reader.ReadToEnd();
-            }
-            response.Close();
-            ParseLinks(responseFromServer, links);
 
-            return newLinks;
+                WebRequest request = WebRequest.Create(currentLink);
+                WebResponse response = request.GetResponse();
+                if (request.RequestUri.Host != "91.210.252.240")
+                {
+                    return;
+                }
+                validLinksFile.WriteLine("{0} {1} {2}", currentLink, ((HttpWebResponse)response).StatusCode, DateTime.Now);
+                string responseFromServer;
+                using (Stream dataStream = response.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(dataStream);
+                    responseFromServer = reader.ReadToEnd();
+                }
+                response.Close();
+                ParseLinks(responseFromServer, links, newLinks);
+            }
+            catch (WebException e)
+            {
+                invalidLinksFile.WriteLine("{0} {1} {2}", currentLink, ((HttpWebResponse)e.Response).StatusCode, DateTime.Now);
+            }             
         }
 
         static void Main(string[] args)
         {
             List<string> links = new List<string>();
+            List<string> newLinks = new List<string>();
             links.Add(root);
-            Request(root, links);
-
-            Console.WriteLine("Hello World!");
+            newLinks.Add(root);
+            using (StreamWriter validLinksFile = new StreamWriter(@"..\..\..\..\valid.txt"))
+            {
+                using (StreamWriter invalidLinksFile = new StreamWriter(@"..\..\..\..\invalid.txt"))
+                {
+                    while (newLinks.Count != 0)
+                    {
+                        string currentLink = newLinks[0];
+                        newLinks.RemoveAt(0);
+                        Request(currentLink, links, newLinks, validLinksFile, invalidLinksFile);
+                    }
+                }
+            }
+            
         }
     }
 }
